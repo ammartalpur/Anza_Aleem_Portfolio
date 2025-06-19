f5b63d30ed9ba50569ae382065416934b72bc928
Anza Aleem <106549351+anzaaleem@users.noreply.github.com>
2025-06-19 10:16:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import initScrollReveal from "/src/scripts/scrollReveal";
 import initTiltEffect from "/src/scripts/tiltAnimation";
 import { targetElements, defaultProps } from "/src/data/scrollRevealConfig";
-
+import "./styles.scss"
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
