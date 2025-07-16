29f216738ce20e4eeca20f3d876b9974ef8b67c6
ammartalpur <ammartalpur69@gmail.com>
2025-07-16 21:38:39 +0500

    Fix stylesheet import syntax in index.js and remove unused stylesheet link in index.html

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import initScrollReveal from "/src/scripts/scrollReveal";
 import initTiltEffect from "/src/scripts/tiltAnimation";
 import { targetElements, defaultProps } from "/src/data/scrollRevealConfig";
-import "./styles.scss"
+
+import "./styles.scss";
+
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
